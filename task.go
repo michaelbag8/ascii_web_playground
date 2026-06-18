27f923276340b95ec23f04242252d450ad6a0104
michaelbag8 <michaelbag8@gmail.com>
2026-06-18 13:17:03 +0100

    print to the terminal

--- task.go
+++ task.go
@@ -11,67 +11,67 @@ import (
 var temp = template.Must(template.ParseFiles("temp/index.html"))
 var tempest = template.Must(template.ParseFiles("temp/layout.html"))
 
-type Book struct{
-	Title string
+type Book struct {
+	Title  string
 	Author string
 }
 
-
-func bookHandler(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/books"{
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/books" {
 		log.Println("wrong path")
 	}
 
-
 	books := []Book{
-	{
-		Title:  "Golang For Idiots",
-		Author: "Michael Bag",
-	},
-	{
-		Title:  "JS Playground For Dummies",
-		Author: "James Baba",
-	},
-	{
-		Title:  "Great To Good",
-		Author: "Stephen Covey",
-	},
-}
-	if err := temp.Execute(w, books); err!=nil{
+		{
+			Title:  "Golang For Idiots",
+			Author: "Michael Bag",
+		},
+		{
+			Title:  "JS Playground For Dummies",
+			Author: "James Baba",
+		},
+		{
+			Title:  "Great To Good",
+			Author: "Stephen Covey",
+		},
+	}
+	if err := temp.Execute(w, books); err != nil {
 		log.Println("error reading books")
 	}
 }
 
-type Greet struct{
+type Greet struct {
 	Name string
 }
-func greetHandler(w http.ResponseWriter, r *http.Request){
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
 	parameters := r.URL.Query().Get("name")
 
-	if parameters != ""{
+	if parameters != "" {
 		fmt.Fprintf(w, "Hello, %s\n", parameters)
 		return
-	}else{
+	} else {
 		fmt.Fprintln(w, "Hello , Guest!")
 	}
-	
+
 }
 
 type User struct {
-    ID    int
-    Name  string
-    Email string
+	ID    int
+	Name  string
+	Email string
 }
-func userHandler(w http.ResponseWriter, r *http.Request){
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	users := User{
-		ID: 1,
-		Name: "John Doe",
+		ID:    1,
+		Name:  "John Doe",
 		Email: "michaelbag8@gmail.com",
 	}
 
-	if err := json.NewEncoder(w).Encode(users); err!=nil{
+	if err := json.NewEncoder(w).Encode(users); err != nil {
 		http.Error(w, "Bad request", http.StatusInternalServerError)
 		return
 	}
@@ -79,18 +79,18 @@ func userHandler(w http.ResponseWriter, r *http.Request){
 }
 
 type Users struct {
-    Name string
-    Age  int
+	Name string
+	Age  int
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method != "POST"{
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	var req Users
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err!=nil{
+	if err != nil {
 		http.Error(w, "Bad request", http.StatusInternalServerError)
 		return
 	}
