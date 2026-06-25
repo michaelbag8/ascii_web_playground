535a61ebda4714c0f6a8753ae384758466ad5ff1
michaelbag8 <michaelbag8@gmail.com>
2026-06-25 21:50:51 +0100

    web switch banner project added

---
+++ Web_Switch_Banner/handlers.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type PageData struct {
+	Result string
+	Banner string
+	Text   string
+}
+
+var templ = template.Must(template.ParseFiles("templates/index.html"))
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "page not found", http.StatusNotFound)
+		return
+	}
+	err := templ.Execute(w, PageData{})
+	if err != nil {
+		http.Error(w, "failed to render template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleAsciiArt(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "failed to parse form", http.StatusInternalServerError)
+		return
+	}
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
+
+	if text == "" {
+		http.Error(w, "text field is empty", http.StatusBadRequest)
+		return
+	}
+
+	for _, ch := range text {
+        if ch < 32 || ch > 126 {
+            http.Error(w, "non ASCII character detected", http.StatusBadRequest)
+            return
+        }
+    }
+
+	if banner == "" {
+		banner = "standard"
+	}
+
+	bannerFile := "banners/" + banner + ".txt"
+	data, err := LoadBanner(bannerFile)
+	if err != nil {
+		http.Error(w, "loading banner file failed", http.StatusInternalServerError)
+		return
+	}
+	result := GenerateArt(text, data)
+
+	final := PageData{
+		Result: result,
+		Banner: banner,
+		Text:   text,
+	}
+
+	err = templ.Execute(w, final)
+	if err != nil {
+		http.Error(w, "failed to render template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleSwitch(w http.ResponseWriter, r *http.Request) {
+	text := r.URL.Query().Get("text")
+	banner := r.URL.Query().Get("banner")
+
+	if text == "" {
+		http.Error(w, "text field is empty", http.StatusBadRequest)
+		return
+	}
+	for _, ch := range text {
+        if ch < 32 || ch > 126 {
+            http.Error(w, "non ASCII character detected", http.StatusBadRequest)
+            return
+        }
+    }
+	
+	if banner == "" {
+		banner = "standard"
+	}
+
+	bannerFile := "banners/" + banner + ".txt"
+	data, err := LoadBanner(bannerFile)
+	if err != nil {
+		http.Error(w, "loading banner file failed", http.StatusInternalServerError)
+		return
+	}
+	result := GenerateArt(text, data)
+
+	final := PageData{
+		Result: result,
+		Banner: banner,
+		Text:   text,
+	}
+
+	err = templ.Execute(w, final)
+	if err != nil {
+		http.Error(w, "failed to render template", http.StatusInternalServerError)
+		return
+	}
+}
---
+++ Web_Switch_Banner/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func LoadBanner(filename string) (map[rune][]string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	content := strings.ReplaceAll(string(data), "\r\n", "\n")
+	rawFile := strings.Split(content, "\n\n")
+
+	bannerMap := make(map[rune][]string)
+	charCode := ' '
+
+	for _, raw := range rawFile {
+		lines := strings.Split(raw, "\n")
+		if len(lines) < 8 {
+			continue
+		}
+		bannerMap[charCode] = lines[:8]
+		charCode++
+	}
+	return bannerMap, nil
+}
+
+func RenderLines(input string, banner map[rune][]string) []string {
+	var output []string
+	for row := 0; row < 8; row++ {
+		var result strings.Builder
+		for _, ch := range input {
+			if lines, ok := banner[ch]; ok && len(lines) > row {
+				result.WriteString(lines[row])
+			}
+		}
+		output = append(output, result.String())
+	}
+	return output
+}
+
+func GenerateArt(input string, banner map[rune][]string) string {
+	var result strings.Builder
+
+	lines := strings.Split(input, "\\n")
+	for _, line := range lines {
+		if line == "" {
+			result.WriteString("\n")
+			continue
+		}
+		content := RenderLines(line, banner)
+		for _, row := range content {
+			result.WriteString(row)
+			result.WriteString("\n")
+		}
+		
+	}
+	return result.String()
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", handleHome)
+	mux.HandleFunc("POST /ascii-art", handleAsciiArt)
+	mux.HandleFunc("GET /ascii-art-switch", handleSwitch)
+
+	fmt.Println("server is running at http://localhost:8080/")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
