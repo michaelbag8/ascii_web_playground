bc97a9b73391973dddc62703d31ad479ffe4acbb
michaelbag8 <michaelbag8@gmail.com>
2026-07-17 14:21:11 +0100

    mutex added

--- advanced/concurrency.go
+++ advanced/concurrency.go
@@ -21,23 +21,46 @@ func printNumbers(wg *sync.WaitGroup) {
 
 }
 
-func square(wg *sync.WaitGroup){
+func square(wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i:=1; i<=10; i++{
+	for i := 1; i <= 10; i++ {
 		fmt.Printf("Square of %d == %d\n", i, i*i)
 	}
 }
+
+var GFG = 0
+
+func work(wg *sync.WaitGroup, m *sync.Mutex) {
+	defer wg.Done()
+	m.Lock()
+	GFG = GFG + 1
+	m.Unlock()
+
+}
+
 func main() {
-	var wg sync.WaitGroup
+
 	
-	wg.Add(3)
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
+	wg.Add(1)
 	go printNumbers(&wg)
+
+	wg.Add(1)
 	go printLetters(&wg)
+
+	wg.Add(1)
 	go square(&wg)
+
+	for i := 0; i < 10; i++ {
+		{
+			wg.Add(1)
+			go work(&wg, &m)
+		}
+	}
 
 	wg.Wait()
 	fmt.Println("main finished")
+	fmt.Println("Value of x", GFG)
 }
-
-
-
